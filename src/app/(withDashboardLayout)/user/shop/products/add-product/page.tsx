98a9bac31d3dcd2cb41c8914fe8d722a2e0fecf3
Masum Billah <masumbillah2062003@gmail.com>
2025-09-06 09:56:39 +0600

    product creating form

---
+++ src/app/(withDashboardLayout)/user/shop/products/add-product/page.tsx
@@ -0,0 +1,11 @@
+import CreateProductForm from "@/components/modules/shop/products/CreateProductForm";
+
+const CreateProductPage = () => {
+    return (
+        <div>
+            <CreateProductForm></CreateProductForm>
+        </div>
+    );
+};
+
+export default CreateProductPage;
---
+++ src/app/(withDashboardLayout)/user/shop/products/page.tsx
@@ -0,0 +1,12 @@
+import ManageProducts from '@/components/modules/shop/products';
+import React from 'react';
+
+const ManageProductsPage = () => {
+    return (
+        <div>
+            <ManageProducts></ManageProducts>
+        </div>
+    );
+};
+
+export default ManageProductsPage;
